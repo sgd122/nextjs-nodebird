cd081915e1946e4e5a1cd6d511b802388e0c5477
Seong Gi Dong <sgd0947@gmail.com>
2022-09-13 20:51:12 +0900

    ✨ feat(images-zoom): add package to react-slick

--- front/components/PostImages.tsx
+++ front/components/PostImages.tsx
@@ -1,6 +1,7 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { PostImage } from 'api';
 import React, { useCallback, useState } from 'react';
+import ImagesZoom from './ImagesZoom';
 
 interface Props {
   images: PostImage[];
@@ -10,15 +11,21 @@ const PostImages: React.FC<Props> = ({ images }) => {
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
   }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
 
   if (images.length === 1) {
     return (
-      <img
-        role="presentation"
-        src={images[0].src}
-        alt={images[0].src}
-        onClick={onZoom}
-      />
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
     );
   }
   if (images.length === 2) {
@@ -38,34 +45,38 @@ const PostImages: React.FC<Props> = ({ images }) => {
           alt={images[1].src}
           onClick={onZoom}
         />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
   return (
-    <div>
-      <img
-        role="presentation"
-        width="50%"
-        src={images[0].src}
-        alt={images[0].src}
-        onClick={onZoom}
-      />
-      <div
-        role="presentation"
-        style={{
-          display: 'inline-block',
-          width: '50%',
-          textAlign: 'center',
-          verticalAlign: 'middle',
-        }}
-        onClick={onZoom}
-      >
-        <PlusOutlined />
-        <br />
-        {images.length - 1}
-        개의 사진 더보기
+    <>
+      <div>
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: 'inline-block',
+            width: '50%',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
       </div>
-    </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
   );
 };
 
